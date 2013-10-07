63e1ab36bcc06a6ee9c0839b038c509ea7c58be1
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-07 13:50:55 +0200

    Removed slides with Runner info

--- app/test/helper/customMatchers.js
+++
@@ -1,9 +0,0 @@
-beforeEach(function() {
-  this.addMatchers({
-    toContainInEl: function(expected) {
-      var html = this.actual.el.html();
-      this.message = "Expected to find '" + expected + "' in '" + html + "'";
-      return html.indexOf(expected) !== -1;
-    }
-  });
-});
